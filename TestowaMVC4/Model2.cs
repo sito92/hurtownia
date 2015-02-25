a25945cdf3cd0722be94253f005870588b1ea71a
Artur Stelmach <arturstelmach92@gmail.com>
2015-02-25 19:57:25 +0100

    added tes project

---
+++ TestowaMVC4/Model2.cs
@@ -0,0 +1,31 @@
+namespace TestowaMVC4
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Model2 : DbContext
+    {
+        // Your context has been configured to use a 'Model2' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'TestowaMVC4.Model2' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'Model2' 
+        // connection string in the application configuration file.
+        public Model2()
+            : base("name=Model2")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
