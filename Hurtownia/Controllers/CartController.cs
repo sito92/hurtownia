c767b625b7f96070c86cf73396ef0fd2ff32b20a
Artur Stelmach <arturstelmach92@gmail.com>
2015-04-07 19:43:21 +0200

    cart in progress

---
+++ Hurtownia/Controllers/CartController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hurtownia.Models;
+using Hurtownia.Repository;
+
+namespace Hurtownia.Controllers
+{
+    public class CartController : Controller
+    {
+        //
+        // GET: /Cart/
+        ProductRepository productRepository = new ProductRepository();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Show()
+        {
+            if (Session["Cart"] == null)
+            {
+                Session["Cart"] = new Cart();
+            }
+            return View(Session["Cart"]);
+        }
+
+        public ActionResult AddProduct(int productId=0)
+        {
+            if (Session["Cart"]==null)
+            {
+                Session["Cart"] = new Cart();
+            }
+            if (productRepository.FindBy(x=>x.Id == productId) != null && productId >0)
+            {
+                ((Cart)Session["Cart"]).Products.Add(productRepository.FindBy(x=>x.Id == productId).FirstOrDefault());
+            }
+            else
+            {
+                TempData["message"] = "Produkt nie znaleziony";
+            }
+            return View("Show",Session["Cart"]);
+        }
+    }
+}
---
+++ Hurtownia/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hurtownia.Models
+{
+    public class Cart
+    {
+        public Cart()
+        {
+            Products = new List<Product>();
+        }
+        public List<Product> Products { get; set; }
+    }
+}
