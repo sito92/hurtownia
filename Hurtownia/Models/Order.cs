e65c8126fe7e39edfff5d1a99f60a38d62710d4a
Kamil Ślusarczyk <kamil.slusarczyk@hotmail.com>
2015-04-13 13:05:22 +0200

    Part of views changes

--- Hurtownia/Models/Order.cs
+++ Hurtownia/Models/Order.cs
@@ -1,16 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 namespace Hurtownia.Models
 {
     public class Order
     {
         public int Id { get; set; }
 
+        [Range(1, Int32.MaxValue, ErrorMessage = "Musisz podać konkretnego klienta")]
         [Display(Name = "Klient")]
         public int ClientId { get; set; }
 
+        [Range(1, Int32.MaxValue, ErrorMessage = "Musisz podać konkretny typ jednostki")]
         [Display(Name = "Typ płatności")]
         public int PaymentTypeId { get; set; }
 
+        [Range(1, Int32.MaxValue, ErrorMessage = "Musisz podać konkretnego pracownika")]
         [Display(Name = "Pracownik")]
         public int EmployeeId { get; set; }
 
